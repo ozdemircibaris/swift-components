e38e960802c84f6b1577adb093876ba718db8ad3
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-12-08 15:31:05 +0300

    project created

---
+++ swiftComponents/swiftComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftComponentsApp.swift
+//  swiftComponents
+//
+//  Created by 10lift on 8.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
